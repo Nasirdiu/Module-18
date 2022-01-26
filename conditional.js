de10ce820838df80cabbbbd885cbaebdc3af2b33
batch-05 <nasir.joypurhat@gmail.com>
2022-01-27 00:37:23 +0600

    if else

---
+++ conditional.js
@@ -0,0 +1,22 @@
+var isFood= true; 
+
+//if condition(){}
+
+if(isFood == true){
+
+    console.log('Mama ami khabo');
+
+}
+
+var prize= 4000;
+
+var mybagut=20000; //jodi beshi hoi taile our put asbe na
+
+if(prize > mybagut){
+    console.log('ami ph kinto iphone buget er mode hola');
+}
+
+else{ 
+
+    console.log('ami kinbo samsung phone')
+}
