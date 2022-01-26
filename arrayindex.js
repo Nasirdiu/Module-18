102fb8a87c3e849535aeb0fbcbd337bbd03d246c
batch-05 <nasir.joypurhat@gmail.com>
2022-01-26 23:10:12 +0600

    indexof()

---
+++ arrayindex.js
@@ -0,0 +1,26 @@
+var bookpage=['hablo','bload','pallob','battery','camical','phices'];
+
+var index=bookpage.indexOf('battery');
+
+console.log(index);
+
+// .............................................num............num....
+var num=[10,20,30,40,50,60,70,80,90,100,101];
+
+var indexnum= num.indexOf(200);
+console.log(indexnum);
+
+
+
+// ..................firstbook...firstbook..........
+var book=['hablo','bload','pallob','battery','camical','phices'];
+var firstbook=book[5];
+console.log(firstbook);
+
+// ......................number
+
+var numbers=[10,20,30,40,50,60,70,80,90,100,101];
+console.log(numbers);
+numbers[1]=22;
+numbers[5]=66;
+console.log(numbers);
