f9b6f4d11929c93f1faf5a5d82cda8ecc025486a
batch-05 <nasir.joypurhat@gmail.com>
2022-01-27 00:51:16 +0600

    if else

---
+++ multipole-condition.js
@@ -0,0 +1,29 @@
+var getJob=true;
+var monySave=15;
+var hous =false;
+
+// if(getJob == true && monySave > 20){
+
+//     console.log('cholo biye korbo tmy jaba ');
+// }
+
+// else{
+//     console.log('amar shate tor biye hba na ja.')
+// }
+
+// if(getJob == true ||  monySave > 20){
+
+//     console.log('cholo biye korbo tmy jaba ');
+// }
+
+// else{
+//     console.log('amar shate tor biye hba na ja.')
+// }
+if((getJob == true &&  monySave > 20) || hous == true) {
+
+    console.log('cholo biye korbo tmy jaba ');
+}
+
+else{
+    console.log('amar shate tor biye hba na ja.')
+}
