84888a98f394ff8314971c58395667e5473b5748
batch-05 <nasir.joypurhat@gmail.com>
2022-01-27 01:06:44 +0600

    contion

---
+++ nesting.js
@@ -0,0 +1,29 @@
+var denish=50;
+var bater=20;
+var tost=10;
+var myBuget=55;
+var khamo= true;
+
+// if(denish<myBuget){
+//     console.log('ami denish cha khabo');
+// }
+
+// else if( bater<myBuget){
+//     console.log('bater diea khabo ajke');
+// }
+// else if(tost<myBuget){
+//     console.log('tost diea khamu ajke ki hoisa bal')
+// }
+
+// else{
+//     console.log('ajke cha khamo na ja tk nai')
+// }
+
+if (denish<myBuget){
+    if( khamo == true){
+        console.log('danish diea khamo')
+    }
+    else{
+        console.log('ami khabo na ja mashi pore gase');
+    }
+}
