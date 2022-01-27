80fdba21a24103b932fb8ea382eee1dbaf0f3390
batch-05 <nasir.joypurhat@gmail.com>
2022-01-27 12:32:15 +0600

    project

---
+++ project.js
@@ -0,0 +1,20 @@
+var result=60;
+
+if(result>= 90){
+    console.log('A+')
+}
+else if(result>= 80){
+    console.log('A')
+}
+else if(result>= 70){
+    console.log('b')
+}
+else if(result>= 60){
+    console.log('c')
+}
+else if(result>= 50){
+    console.log('d')
+}
+else {
+    console.log('Fall F')
+}
